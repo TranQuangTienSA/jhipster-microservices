ed62eeb49ca9e1fdf06855537b50679156c4d962
Trần Quang Tiến <tientq@dkt.com.vn>
2017-05-08 09:16:29 +0700

    query dsl pom

--- customer-service/src/main/java/net/tinyset/customer/repository/CustomerRepository.java
+++ customer-service/src/main/java/net/tinyset/customer/repository/CustomerRepository.java
@@ -1,15 +1,13 @@
 package net.tinyset.customer.repository;
 
 import net.tinyset.customer.domain.Customer;
-
-import org.springframework.data.jpa.repository.*;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
 /**
  * Spring Data JPA repository for the Customer entity.
  */
 @SuppressWarnings("unused")
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long>, QueryDslPredicateExecutor<Customer> {
 
 }
